4dd2d7160aa40461880eef5f6c1890791829e899
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-04-29 13:21:04 -0300

    Foi criado o arquivo Exercício 8

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,19 @@
+﻿    /*
+    Exercício 8: Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. 
+    A fórmula da conversão é F=(9*C+160)/5.
+     */
+
+
+    Console.WriteLine("Exercício 8: Ler uma temperatura em graus Celsius e apresentá-la convertida\n" +
+        " em graus Fahrenheit. A fórmula da conversão é F = (9 * C + 160) / 5.\n");
+
+    int tempC;
+    int tempF;
+
+    Console.Write("Digite a temperatura em graus Celsius: ");
+    tempC = int.Parse(Console.ReadLine());
+
+    tempF = (9 * tempC + 160) / 5;
+    Console.WriteLine("A temperarura convertida em graus Fahrenheit é: " + tempF + "º");
+
+
