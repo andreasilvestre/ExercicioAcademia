30898b16b3487c5fee004c7c30dfa9600e07b62b
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-05 12:28:16 -0300

    Atualização dos exemplos e exercicios de 1 a 5

--- EstruturasCondicionais/Program.cs
+++ EstruturasCondicionais/Program.cs
@@ -172,12 +172,66 @@
 //}
 
 
-Console.WriteLine("4 - Escrever um algoritmo para ler a quantidade de horas aula " +
-    "\n dadas por dois professores e o valor por hora recebido por cada um deles." +
-    "\n  Mostrar na tela qual dos professores tem o maior salário total.");
+//Console.WriteLine("4 - Escrever um algoritmo para ler a quantidade de horas aula " +
+//    "\n dadas por dois professores e o valor por hora recebido por cada um deles." +
+//    "\n Mostrar na tela qual dos professores tem o maior salário total.");
 
-int qt_horaA, qt_horaB;
-double vl_horaA, vl_horaB;
+////leitura
+//int qt_horaA, qt_horaB;
+//double vl_horaA, vl_horaB;
+
+////resultado
+//string professores;
+//string[] valor;
+
+//double salarioA, salarioB;
+
+
+//int n;
+//Console.WriteLine("Digite o total de testes a realizara: ");
+//n = int.Parse(Console.ReadLine());
+
+//for (int i = 0; i < n; i++)
+//{
+//    Console.WriteLine("\nDigite a quantidade de horas dos dois professores" +
+//    "\n e o valor hora de cada um, separado por espaço:");
+//    professores = Console.ReadLine();   
+
+//    valor = professores.Split(' ');
+//    qt_horaA = int.Parse(valor[0]);
+//    qt_horaB = int.Parse(valor[1]);
+//    vl_horaA = double.Parse(valor[2]);
+//    vl_horaB = double.Parse(valor[3]);
+
+//    Console.WriteLine(qt_horaA + " | " + qt_horaB + " | " + vl_horaA + " | " + vl_horaB); //leitura apenas
+
+//    salarioA = qt_horaA * vl_horaA;
+//    salarioB = qt_horaB * vl_horaB;
+
+//    Console.WriteLine("Os salários dos professores A e B são: ");
+//    Console.WriteLine(salarioA.ToString("C") + " | " + salarioB.ToString("C"));
+
+//    //o Math.Floor(variavel) e (int)varivel retorna apenas a parte inteira sem arredondar
+//    //Ambos retorna mesmo resulado
+//    //Console.WriteLine("Math.Floor: " + Math.Floor(vl_horaA) + " | " + Math.Floor(vl_horaB));
+//    //Console.WriteLine("int: " + (int)vl_horaA + " | " + (int)vl_horaB); //testando o (int)variavael
+//    //Console.WriteLine(salarioA.ToString("F") + " | " + salarioB.ToString("F")); //testar duas casas decimais
+//    //retorna true se achar a palavra 100, false se não achou
+//    //Console.WriteLine("Texto digitado: " + professores + professores.Contains("100"));
+
+//    if (salarioA > salarioB)
+//    {
+//    }
+//    else if (salarioA == salarioB)
+//    {
+//        Console.WriteLine("Ambos professores tem o mesmo salário.");
+//    }
+//    else
+//    {
+//        Console.WriteLine("O professor B tem o maior salario.");
+//    }
+//}
+// depois fazer summary
 
 
 
@@ -187,3 +241,45 @@ Console.WriteLine("5 - Escrever um algoritmo para ler duas notas de um aluno e "
     "\n deve ler a nota do exame e calcular a média final. Se esta média for " +
     "\n maior ou igual a 5,0, o programa deve escrever “Aprovado”, " +
     "\n caso contrário deve escrever “Reprovado”.");
+
+//notas de 0 a 10
+double nota1;
+double nota2;
+double media;
+
+double nota_exame;
+double media_final;
+
+int n; // informar quantidade de teste
+Console.WriteLine("Digite a quantidade de testes: ");
+n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Digite as duas notas: ");
+    nota1 = double.Parse(Console.ReadLine());
+    nota1 = double.Parse(Console.ReadLine());
+
+    media = (nota1 + nota1) / 2;
+
+    if (media >= 7)
+    {
+        Console.WriteLine("Aprovado");
+    }
+    else
+    {
+        Console.WriteLine("Digite a nota do exame final: ");
+        nota_exame = double.Parse(Console.ReadLine());
+
+        media_final = (media + nota_exame) / 2;
+
+        if (media_final >= 5)
+        {
+            Console.WriteLine("Aprovado");
+        }
+        else
+        {
+            Console.WriteLine("Reprovado");
+        }
+    }
+}
