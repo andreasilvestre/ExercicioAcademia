d6b8fc26ae41eaadcf6972044f8dd42553ca7b59
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-06 17:00:48 -0300

    criei exemplo 4

---
+++ Condicionais_ifelse/exemplo4/Program.cs
@@ -0,0 +1,18 @@
+﻿// exemplo 4
+int num;
+
+Console.WriteLine("Digite um número qualquer: ");
+num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Divisível por 2");
+}
+if (num % 4 == 0)
+{
+    Console.WriteLine("Divisível por 4");
+}
+if (num % 8 == 0)
+{
+    Console.WriteLine("Divisível por 8");
+}
