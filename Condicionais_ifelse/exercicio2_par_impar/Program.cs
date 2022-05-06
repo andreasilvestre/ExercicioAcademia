eba3c5d1bb5d91dd6099e3ab6582ac51501f99fb
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-06 17:23:07 -0300

    exercicio 2 num par ou impar

---
+++ Condicionais_ifelse/exercicio2_par_impar/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Exercício 2 - Faça um algoritmo para ler um número e verificar se ele é par ou ímpar.");
+
+/// <summary>
+/// guarda o valor de um número qualquer digitado pelo usuário
+/// </summary>
+double numero;
+
+Console.WriteLine("Digite um número qualquer: ");
+numero = double.Parse(Console.ReadLine());
+
+if (numero % 2 == 0) Console.WriteLine("Número par.");
+else Console.WriteLine("Número ímpar.");
+
+
