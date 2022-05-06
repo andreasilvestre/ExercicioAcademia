496cc19606a3828ffbb11f5fdc81790f549fdd35
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-06 18:11:02 -0300

    Dado um tamanho de arquivo e velocidade da internet, calcula o tempo estimado de download

---
+++ Condicionais_ifelse/exercicio14_TempoDownload/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine(" 14 - Faça um programa que peça o tamanho de um arquivo para download" +
+    "\n (em MB) e a velocidade de um link de Internet (em Mbps). Em seguida," +
+    "\n calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).");
+
+/// <summary>
+/// armazena o tamanho do arquivo a ser baixado
+/// </summary>
+int tamanho_arquivo;
+
+/// <summary>
+/// armazena a velocidade da internet para o link informado
+/// </summary>
+int veloc_internet;
+
+/// <summary>
+/// guarda quanto tempo aproximado levará para o arquivo ser baixado
+/// </summary>
+double tempo_gasto;
+
+Console.WriteLine("Digite o tamanho do arquivo e a velocidade da internet: ");
+tamanho_arquivo = int.Parse(Console.ReadLine());
+veloc_internet = int.Parse(Console.ReadLine());
+
+tempo_gasto = tamanho_arquivo / veloc_internet;
+tempo_gasto = tempo_gasto / 60;
+
+Console.WriteLine(Math.Ceiling(tempo_gasto) + " minutos");
