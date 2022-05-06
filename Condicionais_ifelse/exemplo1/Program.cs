4261572b29cc39bd67a7ae8e3eac269b9b05cd27
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-06 16:54:14 -0300

    criei exemplo 1

---
+++ Condicionais_ifelse/exemplo1/Program.cs
@@ -0,0 +1,21 @@
+﻿//exemplo 1
+
+int num;
+
+Console.WriteLine("Digite um número qualquer: ");
+num = int.Parse(Console.ReadLine());
+
+if (num > 0)
+{
+    Console.WriteLine("Positivo");
+}
+else if (num < 0)
+{
+    Console.WriteLine("Negativo");
+}
+else
+{
+    Console.WriteLine("Zero");
+}
+
+//if (num > 0) Console.WriteLine("Positivo - Você pode escrever if assim se for apenas uma linha");
