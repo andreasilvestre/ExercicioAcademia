1debf2aebdaa32c4cfa39f55bbbf4e16ae7d2059
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-06 17:13:28 -0300

    exemplo 12 calculadora

---
+++ Condicionais_ifelse/exemplo12_calculadora/Program.cs
@@ -0,0 +1,41 @@
+﻿//exemplo 12 - fazendo calculadora
+double num1, num2, resultado;
+int op;
+
+Console.WriteLine("Digite dois números: ");
+num1 = double.Parse(Console.ReadLine());
+num2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a operação que deseja fazer: " +
+    "\n1 - Soma" +
+    "\n2 - Subtração" +
+    "\n3 - Multiplicação" +
+    "\n4 - Divisão" +
+    "\n5 - Potência");
+op = int.Parse(Console.ReadLine());
+
+if (op == 1)
+{
+    resultado = num1 + num2;
+    Console.WriteLine("Soma: " + resultado);
+}
+else if (op == 2)
+{
+    resultado = num1 - num2;
+    Console.WriteLine("Subtração: " + resultado);
+}
+else if (op == 3)
+{
+    resultado = num1 * num2;
+    Console.WriteLine("Multiplicação: " + resultado);
+}
+else if (op == 4)
+{
+    resultado = num1 / num2;
+    Console.WriteLine("Divisão: " + resultado);
+}
+else if (op == 5)
+{
+    resultado = Math.Pow(num1, num2);
+    Console.WriteLine("Potência: " + resultado);
+}
