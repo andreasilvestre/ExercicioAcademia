de0b9988d1bac81f7dc626cc2df8422b44a90063
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-13 12:49:58 -0300

    fiz até o exercicio 16

--- Lacos_Repeticao/exemplos/Program.cs
+++ Lacos_Repeticao/exemplos/Program.cs
@@ -332,3 +332,501 @@ Ao final, informe o nome do candidato vencedor, o número de votos nulos e o nú
 ou seja, repita a leitura até que ela esteja correta (maior do que zero). 
 Ao final, mostre a idade digitada.
  */
+
+///// <summary>
+///// guarda a idade digitada pelo usuário
+///// </summary>
+//int idade = 0;
+
+//Console.WriteLine("Digite uma idade: ");
+//idade = int.Parse(Console.ReadLine());
+
+//while (idade <= 0)
+//{
+//    Console.WriteLine("Idade incorreta.");
+//    Console.WriteLine("Digite uma idade válida, maior que zero.");
+//    idade = int.Parse(Console.ReadLine());
+//}
+
+//Console.WriteLine("A idade digitada é: " + idade);
+
+
+
+/*  EXERCICIO07
+ 7. Faça um programa em VS que solicite o nome, a idade e o salário de uma pessoa. 
+A cada solicitação, teste se o usuário informou um valor válido. 
+Por exemplo: se o nome for não for lido corretamente (ou seja, se estiver em branco ou se for um número), 
+informe que ele está incorreto e saia do programa em VS. 
+Se o nome for lido corretamente, solicite a idade. Se ela for incorreta (menor ou igual a zero), 
+informe que está errada e saia. Se estiver correta, solicite o salário. 
+Se ele estiver incorreto (menor ou igual a zero), informe que está incorreto e saia. 
+Se estiver correto, mostre todos os valores lidos.
+ */
+
+
+///// <summary>
+///// guarda o nome da pessoa que foi digitado
+///// </summary>
+//string nome;  //se for branco ou numero, informar q esta incorreto e abortar programa
+
+///// <summary>
+///// verifica se há algum numero digitado no nome
+///// </summary>
+//bool contem_numero = false;
+
+///// <summary>
+///// guarda a idade informada pelo usuário
+///// </summary>
+//int idade;
+
+///// <summary>
+///// guardao valor do salário informado pelo usuário
+///// </summary>
+//double salario;
+
+//Console.WriteLine("Programa de leitura do nome, idade e saláio de uma pessoa");
+//Console.WriteLine("Digite o nome da pessoa: ");
+//nome = Console.ReadLine();
+//nome = nome.ToUpper();
+
+//if ( nome == "" || nome.Contains("0") || nome.Contains("1") || nome.Contains("2") || nome.Contains("3") || nome.Contains("4") || nome.Contains("5") || nome.Contains("6") || nome.Contains("7") || nome.Contains("8") || nome.Contains("9"))
+//{
+//    contem_numero = true;
+//    Console.WriteLine("Nome inválido.");
+//}
+//else
+//{
+//    Console.WriteLine("Digite a idade: ");
+//    idade = int.Parse(Console.ReadLine());
+//    if (idade <= 0)
+//    {
+//        Console.WriteLine("Idade inválida.");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Digite a salário: ");
+//        salario = double.Parse(Console.ReadLine());
+//        if (salario <= 0)
+//        {
+//            Console.WriteLine("Salário inválido");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Os dados lidos são: ");
+//            Console.WriteLine("Nome: " + nome);
+//            Console.WriteLine("Idade: " + idade + " anos");
+//            Console.WriteLine("Salário: " + salario.ToString("C"));
+//        }
+//    }
+//}
+
+
+
+/* EXERCICIO08 - ACHAR NUMERO INTEIRO
+8. Faça um programa em VS que solicite um numero inteiro. Se o numero não for inteiro, solicite-o até 
+que seja. após, informe se ele é par ou impar. 
+Finalmente, pergunte ao usuário se ele quer repetir o programa em VS.
+*/
+
+//num_inteiro = (int)numero;  //pega somente a parte inteira
+
+///// <summary>
+///// guarda o número digitado 
+///// </summary>
+//string numero;
+
+///// <summary>
+/////  Guarda o numero inteiro digitado. Se não for inteiro, guarda 0. 
+///// </summary>
+//int num_inteiro;
+
+///// <summary>
+///// Guarda True ou False. Se número digitado é imteiro, guarda True, senão False.
+///// </summary>
+//bool eh_inteiro = false;
+
+///// <summary>
+///// guarda os caracteres s/n - repetir sim, repetir não.
+///// </summary>
+//char repetir;
+
+//do
+//{
+//    do
+//    {
+//        Console.WriteLine("Digite um número inteiro: ");
+//        numero = Console.ReadLine();
+//        eh_inteiro = int.TryParse(numero, out num_inteiro);
+
+//    } while (eh_inteiro == false);
+
+//    if (num_inteiro%2 == 0)
+//    {
+//        Console.WriteLine("O número informado é Par.");
+//    }
+//    else
+//    {
+//        Console.WriteLine("O número informado é Ímpar.");
+//    }
+
+//    Console.WriteLine("Deseja repetir a análise do número? (s/n)");
+//    repetir = char.Parse(Console.ReadLine());
+//    repetir = char.ToUpper(repetir);
+
+//} while (repetir == 'S');
+
+////verificação de dados:
+//Console.WriteLine("Mostra número convertido em inteiro: " + num_inteiro + " É inteiro? "+ eh_inteiro)
+
+
+/* EXERCICIO09 - INTERVALO DE FILHOS VER FUNÇÃO PRONTA
+9. Faça um programa em VS para Ler a quantidade de filhos de cada uma das 30 pessoas que visitaram 
+o zoológico num determinado dia, imprimindo:
+Quantas pessoas tem entre 1 e 3 filhos.
+Quantas pessoas tem entre 4 e 7 filhos.
+Quantas pessoas tem mais de 8 filhos.
+Quantas pessoas não tem filhos.
+*/
+
+////30 pessoas
+
+///// <summary>
+/////  guarda a quantidade de filhos de uma pessoa informada pelo usuário
+///// </summary>
+//int quantidade_filhos;
+
+///// <summary>
+/////  guarda a quantidade de pessoas que tem de 1 a 3 filhos
+///// </summary>
+//int filhos1a3 = 0;
+
+///// <summary>
+///// guarda a quantidade de pessoas que tem de 4 a 7 filhos
+///// </summary>
+//int filhos4a7 = 0;
+
+///// <summary>
+///// guarda a quantidade de pessoas que tem 8 ou mais filhos
+///// </summary>
+//int filhosMaisde8 = 0;
+
+///// <summary>
+///// guarda a quantidade de pessoas que não possui nenhum filho
+///// </summary>
+//int semFilhos = 0;
+
+//for (int i = 1; i <= 30; i++)
+//{
+//    Console.WriteLine("Digite a quantidade de filhos da pessoa " + i);
+//    quantidade_filhos = int.Parse(Console.ReadLine());   
+
+//    if ((quantidade_filhos == 1) || (quantidade_filhos == 2) || (quantidade_filhos == 3))
+//    {
+//        filhos1a3++;
+//    }
+//    else if ((quantidade_filhos == 4) || (quantidade_filhos == 5) || (quantidade_filhos == 6) || (quantidade_filhos == 7))
+//    {
+//        filhos4a7++;
+//    }
+//    else if (quantidade_filhos >= 8)
+//    {
+//        filhosMaisde8++;
+//    }
+//    else if (quantidade_filhos == 0)
+//    {
+//        semFilhos++;
+//    }
+//}
+
+//Console.WriteLine("O panorama sobre quantidade de filhos dos visitantes ficaram da seguinte forma: ");
+//Console.WriteLine("De 1 a 3 filhos: " + filhos1a3 );
+//Console.WriteLine("De 4 a 7 filhos: " + filhos4a7);
+//Console.WriteLine("Mais de 8 filhos: " + filhosMaisde8);
+//Console.WriteLine("Sem filhos: " + semFilhos);
+
+
+
+
+
+
+///*  EXERCICIO10
+//10. Ler nome, sexo e idade de 50 pessoas e após imprimir:
+//Número de pessoas do sexo masculino.
+//Número de pessoas do sexo feminino.
+//Número de pessoas com idade inferior a 30 anos.
+//Número de pessoas com idade superior a 60 anos.
+//Média de idade das mulheres.
+//*/
+
+////total 50 pessoas
+///// <summary>
+///// guarda o nome da pessoa
+///// </summary>
+//string nome;
+
+///// <summary>
+///// guarda o sexo da pessoa, masculino ou feminino
+///// </summary>
+//string sexo;//feminino ou masculino
+
+///// <summary>
+///// guarda a idade da pessoa
+///// </summary>
+//int idade;
+
+///// <summary>
+///// guarda o total de pessoas do sexo masculino
+///// </summary>
+//int totalMasculino = 0;
+
+///// <summary>
+///// guarda o total de pessoas do sexo feminino
+///// </summary>
+//int totalFeminino = 0;
+
+///// <summary>
+///// guarda o total de pessoas com idade inferior a 30 anos
+///// </summary>
+//int inferior30Anos = 0;
+
+///// <summary>
+///// guarda o total de pessoas com idade superior a 60 anos
+///// </summary>
+//int superior60Anos = 0;
+
+///// <summary>
+///// guarda a somatória das idades das mulheres para calcular a média
+///// </summary>
+//int somaIdadeMulheres = 0; //para achar média
+
+///// <summary>
+///// guarda a média das idades das mulheres => media = soma das idades mulheres / total de idades mulheres
+///// </summary>
+//double mediaIdadeMulheres = 0; // divide por totalFeminino
+
+////pendencia= trocar for para 50 pessoas - teste com 5
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine("Digite o nome da pessoa: ");
+//    nome = Console.ReadLine();
+//    nome = nome.ToUpper();
+
+//    Console.WriteLine("Digite o sexo - (feminino/masculino)");
+//    sexo = Console.ReadLine();
+//    sexo = sexo.ToUpper();
+
+//    Console.WriteLine("Digite a idade: ");
+//    idade = int.Parse(Console.ReadLine());
+
+//    if (sexo == "MASCULINO")
+//    {
+//        totalMasculino++;
+//    }
+//    else if (sexo == "FEMININO")
+//    {
+//        totalFeminino++;
+//        somaIdadeMulheres = somaIdadeMulheres + idade;
+//    }
+
+//    if (idade < 30)
+//    {
+//        inferior30Anos++;
+//    }
+//    else if (idade > 60)
+//    {
+//        superior60Anos++;
+//    }
+//}
+//mediaIdadeMulheres = somaIdadeMulheres / totalFeminino;
+
+//Console.WriteLine("Pessoas do sexo masculino: " + totalMasculino);
+//Console.WriteLine("Pessoas do sexo feminino: " + totalFeminino);
+//Console.WriteLine("Pessoas com idade inferior a 30 anos: " + inferior30Anos);
+//Console.WriteLine("Pessoas com idade superior a 60 anos: " + superior60Anos);
+//Console.WriteLine("Média de idade das mulheres.: " + (int)mediaIdadeMulheres);
+
+
+
+
+///*
+//// EXERCICIO11 - usar list[] LISTA
+//PENDENCIA - VER LISTA COMPOSTA POR MAIS DE UM TIPO. EX NOME E NOTA DOS ALUNOS
+//11: Ler as notas de uma turma de alunos e ao final imprimir a nota mais alta, a nota mais baixa, 
+//a média aritmética da turma e a quantidade de alunos da turma. Usar como flag a nota 12.
+//*/
+
+
+///// <summary>
+///// guarda o nome de cada aluno informado
+///// </summary>
+//List<string> listaAluno = new List<string>(); // naõ está servindo pra nada por enquanto
+
+///// <summary>
+///// guarda a relação de notas dos respectivos alunos informados
+///// </summary>
+//List<double> listaNota = new List<double>(); //toda vez que usar new colocar parenteses () no final
+
+///// <summary>
+///// guarda o total de alunos da turma analisada
+///// </summary>
+//int totalAlunos;
+
+///// <summary>
+///// guarda a somatoria das notas e depois a média final da turma
+///// </summary>
+//double mediaNota = 0;
+
+//Console.WriteLine("Digite a quantidade de Alunos: ");
+//totalAlunos = int.Parse(Console.ReadLine());
+
+//for (int i = 0; i < totalAlunos; i++)
+//{
+//    Console.WriteLine("Digite o nome do aluno: ");
+//    listaAluno.Add(Console.ReadLine());
+
+//    Console.WriteLine("Digite a nota do aluno " + listaAluno[i] + " (nota de 0 a 12)");
+//    listaNota.Add(double.Parse(Console.ReadLine()));
+
+//    mediaNota = mediaNota + listaNota[i];
+//}
+
+//mediaNota = mediaNota / listaNota.Count;
+//listaNota.Sort();// ordena a lista em ordem crescente, de 0 a totalAlunos
+
+//Console.WriteLine("Nota mais alta: " + listaNota[listaNota.Count-1]);
+//Console.WriteLine("Nota mais baixa: " + listaNota[0]);
+//Console.WriteLine("Total de alunos: " + listaNota.Count);
+//Console.WriteLine("A média aritmética da turma é: " + mediaNota);
+
+
+
+///* EXERCICIO12
+//12: Apresentar o total da soma dos cem primeiros números inteiros.
+//*/
+
+//int somaInteiro =0 ; //5050
+
+//// de 1 a 100
+//for (int i = 1; i <= 100; i++)
+//{
+//    somaInteiro = somaInteiro + i;   
+//}
+
+//Console.WriteLine("A soma total dos cem primeiros números inteiros é: " + somaInteiro);
+
+
+
+///* EXERCICIO13
+//13: Elaborar um programa que apresente o somatório dos valores pares existentes entre 1 e 500.
+//*/
+//int somaPares = 0; // resultado 62750
+
+//for (int i = 1; i <= 500; i++)
+//{
+//    if (i%2 == 0)  //resto zero é par
+//    {
+//        somaPares = somaPares + i;
+//    }
+//}
+//Console.WriteLine("O somatório dos valores pares existentes entre 1 e 500 é " + somaPares);
+
+
+
+///*EXERCICIO14
+//14: Mostrar as potências de 2 variando de 0 a 10.
+//*/
+
+//for (int i = 0; i <= 10; i++)
+//{
+//    Console.WriteLine("Potência de 2 elevado à " + i + " é: " + Math.Pow(2,i));
+//}
+
+
+///* EXERCICIO15 - MEDIA PONDERADA
+//15: Ler 4 valores referentes a notas escolares de um aluno e imprimir uma mensagem dizendo se ele está aprovado 
+//ou reprovado, após ter calculado sua média ponderando, considerando pesos 2, 1, 2, e 4, respectivamente. 
+//Considerar nota 7,0 como mínima para aprovação.
+//*/
+
+//List<double> listaNota = new List<double>();
+//double mediaPonderada;
+
+//for (int i = 0; i < 4; i++)
+//{
+//    Console.WriteLine("Digite a nota (de 0 a 10):");
+//    listaNota.Add(double.Parse(Console.ReadLine()));
+
+//}
+
+///*"A média ponderada é calculada por meio do somatório das multiplicações entre valores e pesos" +
+//    " divididos pelo somatório dos pesos."
+//*/
+////Veja mais em: https://brasilescola.uol.com.br/matematica/media-ponderada.htm 
+
+//mediaPonderada = (listaNota[0] * 2 + listaNota[1] * 1 + listaNota[2] * 2 + listaNota[3] * 4) / (2 + 1 + 2 + 4);
+////divide por 9
+
+//Console.WriteLine("A média ponderada é: " + mediaPonderada.ToString("F"));
+//if (mediaPonderada >= 7)
+//{
+//    Console.WriteLine("Aprovado");
+//}
+//else
+//{
+//    Console.WriteLine("Reprovado");
+//}
+
+
+///*
+//16: Ler um número N qualquer menor ou igual a 50 e apresentar o valor obtido da multiplicação sucessiva de N 
+//por 3 enquanto o produto for menor que 250 (N*3; N*3*3; N*3*3*3; etc).
+//*/
+//int numero = 0; // de 0 a 50 por exemplo
+//int produto = 0; //numero * 3, numero * 3 * 3, 
+//do
+//{
+//    Console.WriteLine("Digite um número de 1 a 50: ");
+//    numero = int.Parse(Console.ReadLine());
+
+//} while (numero > 50);
+
+//produto = numero;
+
+//do
+//{
+//    produto = produto * 3;
+//} while (produto < 250);
+
+//Console.WriteLine("O resultado final é: " + produto);
+
+
+
+/*
+17: Apresentar os quadrados dos números inteiros de 15 a 200.
+
+18: Elaborar um programa que apresente o valor de uma potência de uma base qualquer elevada a um expoente 
+qualquer, ou seja, NM.
+
+19. A prefeitura de uma cidade fez uma pesquisa entre seus habitantes, coletando dados sobre o salário e número 
+de filhos. A prefeitura deseja saber: 
+ 
+a) média do salário da população;
+b) média do número de filhos;
+c) maior salário;
+d) percentual de pessoas com salário até R$ 100,00.
+O final da leitura de dados se dará com a entrada de um salário negativo.
+
+20. Foi realizada uma pesquisa de algumas características físicas da população de uma certa região, a qual coletou os seguintes dados referentes a cada habitante para serem analisados:
+- sexo (masculino e feminino)
+- cor dos olhos (azuis, verdes ou castanhos)
+- cor dos cabelos ( louros, castanhos, pretos)
+- idade 
+ 
+21. Faça um algoritmo que determine e escreva: 
+ 
+- a maior idade dos habitantes
+- a quantidade de indivíduos do sexo feminino cuja idade está entre 18 e 35 anos inclusive
+e que tenham olhos verdes e cabelos louros.
+O final do conjunto de habitantes é reconhecido pelo valor -1 entrada como idade.
+ 
+ */
