a85a40211bf542cb389df950cff5e19d0a7ebd8d
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-17 12:01:10 -0300

    atualizando tudo

---
+++ Classes_exemplos/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_exemplos
+{
+    public class Class1
+    {
+        // os metodos Equals, ToString e GetHashCode estão na classe pronta do C# chamada object
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
