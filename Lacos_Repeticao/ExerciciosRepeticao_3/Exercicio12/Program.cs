82a08c6b5d78a99b3fd304d2a647b425325fab22
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-17 16:17:47 -0300

    soma de 1 a 100

---
+++ Lacos_Repeticao/ExerciciosRepeticao_3/Exercicio12/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("EXERCICIO12" +
+"\n 12: Apresentar o total da soma dos cem primeiros números inteiros.\n");
+
+int somaInteiro = 0; 
+
+// de 1 a 100
+for (int i = 1; i <= 100; i++)
+{
+    somaInteiro = somaInteiro + i;
+}
+
+Console.WriteLine("A soma total dos cem primeiros números inteiros é: " + somaInteiro); //soma = 5050
