c4cea7fd26f51f702d323fb3183d998be9151095
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-17 14:14:41 -0300

    valida idade

---
+++ Lacos_Repeticao/ExerciciosRepeticao_3/Exercicio06/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("EXERCICIO06" +
+"\n 6. Faça um programa em VS que solicite ao o usuário que informe a idade e valide a entrada de dados," +
+"\n ou seja, repita a leitura até que ela esteja correta (maior do que zero)." +
+"\n Ao final, mostre a idade digitada.");
+
+
+/// <summary>
+/// guarda a idade digitada pelo usuário
+/// </summary>
+int idade = 0;
+
+Console.WriteLine("Digite uma idade: ");
+idade = int.Parse(Console.ReadLine());
+
+while (idade <= 0)
+{
+    Console.WriteLine("Idade incorreta.");
+    Console.WriteLine("Digite uma idade válida, maior que zero.");
+    idade = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("A idade digitada é: " + idade + " anos");
