b28c4465dcd8f935bf12c7f0721ad3eb3468a397
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-20 09:30:04 -0300

    Primeira Classe

---
+++ Manipulacao_Arquivo/Drop6_listaArquivoEscrita/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drop6_listaArquivoEscrita
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public string email; //atributo será usado com identificador
+
+        //método construtor
+        public Pessoa(string n, string e)
+        {
+            nome = n;
+            email = e;
+        }
+    }
+}
---
+++ Manipulacao_Arquivo/Drop6_listaArquivoEscrita/Program.cs
@@ -0,0 +1,110 @@
+﻿//Programa em VS que recebe emails de um usuário e que são gravados em arquivo.
+
+using Drop6_listaArquivoEscrita;
+
+string opcao = "";
+string nomeArquivo = "emails.dat"; //texto puro -> plain text
+
+List<Pessoa> listaPessoas = new List<Pessoa>();
+
+//verificar se o arquivo de dados existe e popular a listaPessoas
+//abrir e ler os dados do arquivo, populando na lista
+try //tratamento de exceções
+{
+    StreamReader leitor = new StreamReader(nomeArquivo); //abre o arquivo para leitura
+    string linha;
+    string[] dados;
+    do
+    {
+        linha = leitor.ReadLine();
+        dados = linha.Split(";");
+        Pessoa p = new Pessoa(dados[0], dados[1]);
+
+        listaPessoas.Add(p);
+
+    } while (!leitor.EndOfStream);
+
+    leitor.Close(); //fecha o objeto que representa o arquivo
+}
+catch (IOException e)
+{
+    //nao faz nada, pq o arquivo será criado na linha 34
+}
+
+StreamWriter escritor = new StreamWriter(nomeArquivo, true); //abre o arquivo para escrita e com append
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("MENU");
+    Console.WriteLine("1 - Cadastrar uma pessoa");
+    Console.WriteLine("2 - Exibir pessoas");
+    Console.WriteLine("3 - Sair");
+
+    Console.Write("Opção: ");
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            //receber um email e gravar no arquivo
+            try
+            {
+                string nome;
+                string email;
+
+                Console.Write("Digite nome: ");
+                nome = Console.ReadLine().ToUpper();
+
+                Console.Write("Digite email: ");
+                email = Console.ReadLine().ToLower();
+
+                //criar um objeto Pessoa com os valores nome e email
+                Pessoa p = new Pessoa(nome, email);
+
+                //verificar se essa dupla já está na lista, logo no arquivo
+                bool jaCadastrado = false;
+                foreach (Pessoa i in listaPessoas)
+                {
+                    if (i.email.Equals(email))
+                    {
+                        Console.WriteLine("Esse email já está no sistema");
+                        jaCadastrado = true;
+                        break;
+                    }
+                }
+                if (!jaCadastrado)
+                {
+                    //adicionar o objeto na lista
+                    listaPessoas.Add(p);
+                    //adicionar o objeto no arquivo
+                    escritor.WriteLine(p.nome + ";" + p.email); //literalmente escreve no procurador e em memória
+                    escritor.Flush(); //garante depois de cada frase digitada que vá para o arquivo                
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao gravar o arquivo: " + e.Message);
+            }
+            break;
+        case "2":
+            Console.WriteLine("Exbindo a lista de pessoas");
+
+            foreach (Pessoa i in listaPessoas)
+            {
+                Console.WriteLine(i.nome + " - " + i.email);
+            }
+
+            break;
+        case "3":
+            Console.WriteLine("Obrigado por usar o sistema");
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+    Console.Write("Pressione algo para continuar!");
+    Console.ReadKey();
+} while (opcao != "3");
+
+escritor.Close(); //fecha o objeto que representa o arquivo
