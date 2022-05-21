3b648594d6f5497db86c184b4b1798513ffb17dc
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-21 09:29:30 -0300

    primeiras classes

---
+++ Carro/Program.cs
@@ -0,0 +1,61 @@
+﻿
+//Crie uma classe Carro com os atributos: modelo, marca, cor e placa. Crie um método
+//para definir os atributos, outro método para ligar o carro e outro método para desligar o carro.
+
+Carro c = new Carro();
+
+c.ligar();
+c.defineAtributos();
+Console.WriteLine("Marca: " + c.marca);
+Console.WriteLine("Modelo: " + c.modelo);
+Console.WriteLine("Cor: " + c.cor);
+Console.WriteLine("Placa: " + c.placa);
+c.desligar();
+
+class Carro
+{
+    public string modelo;
+    public string marca;
+    public string cor;
+    public string placa;
+
+    public bool ligado;
+
+    public void defineAtributos()
+    {
+        modelo = "gol";
+        marca = "Volks";
+        cor = "prata";
+        placa = "dae-7484";
+    }
+
+    public void ligar()
+    {
+        Console.WriteLine("Carro ligado");
+        ligado = true;
+    }
+
+    public void desligar()
+    {
+        Console.WriteLine("Carro desligado");
+        ligado = false;
+    }
+}
+
+
+/*
+ * •Escreva uma classe Pessoa contendo todos os 
+ * atributos de uma pessoa. Faça métodos para apresentar os dados.
+ */
+
+class Pessoa
+{
+    public string nome;
+    public string cpf;
+    public double telefone;
+    public string email;
+    public string endereco;
+
+    public string x()
+    { }
+}
---
+++ ProjetoOO/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Lampada l = new Lampada();
+//l.Ligar();
+//l.potencia = 9;
+Console.WriteLine("Ligado= " + l.ligado + " Potencia= " + l.potencia);
+l.desligar();
+Console.WriteLine("Ligado= " + l.ligado + " Potencia= " + l.potencia);
+double n;
+n = l.potencia;
+Console.WriteLine("A potencia da lampada cadastrada é: " + n);
+Console.WriteLine("Retorno da função estaLigada: " + l.estaligada());
+l.Ligar();
+l.desligar();
+if (l.estaligada())
+{
+Console.WriteLine("Lampada ligada.");
+}
+else
+{
+Console.WriteLine("Lampada desligada");
+}
+
+public class Lampada
+{
+    public bool ligado;
+    public double potencia;
+
+    //public, private, protected
+    public void Ligar()
+    {
+        Console.WriteLine("A lampada está sendo ligada");
+        ligado = true;
+    }
+    public void desligar()
+    {
+        Console.WriteLine("A lampada está sendo desligada");
+        ligado = false;
+    }
+    public double retornaPotencia()
+    {
+        return potencia;
+    }
+
+    public bool estaligada()
+    {
+        return ligado;
+    }
+}
+
