7246c1137e09a7a57aff266d2f55815715563638
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-25 11:00:05 -0300

    exercicio 03 - orientação

---
+++ POO/Exercicio03/Program.cs
@@ -0,0 +1,7 @@
+﻿//#3
+//Os quatro pilares do paradigma de Orientação a Objetos são:
+
+//A - Sequenciamento, Procedimentos, Bibliotecas e Herança.
+//B - Herança, Polimorfismo, Classes e Objetos.
+//C - Classes, Atributos, Métodos e Abstração.
+//D - Abstração, Encapsulamento, Herança e Polimorfismo.
