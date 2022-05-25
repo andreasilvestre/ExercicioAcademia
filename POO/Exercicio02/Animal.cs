ab473efc1128f89545c38da27c29edec27534dcb
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-25 10:19:10 -0300

    exercicio 02 - animais de estimação

---
+++ POO/Exercicio02/Animal.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02
+{
+    class Animal
+    {
+        /// <summary>
+        /// guarda o nome do animal de estimação
+        /// </summary>
+        string nome;
+
+        /// <summary>
+        /// guarda o tipo de animal animal que a pessoa tem,
+        /// neste caso Cachorro, Gato ou Peixe
+        /// </summary>
+        string tipo;
+
+
+        public Animal(string nome, string tipo)
+        {
+            this.nome = nome;
+            this.tipo = tipo;
+        }
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+
+
+        public static bool verificaTipo(string tipo)
+        {
+            if (tipo == "CACHORRO" || tipo == "GATO" || tipo == "PEIXE")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void Quantidade(List<Animal> lista)
+        {
+            int quantidadeCachorro = 0;
+            int quantidadeGato = 0;
+            int quantidadePeixe = 0;
+
+            foreach (Animal i in lista)
+            {
+                if (i.Tipo == "CACHORRO")
+                {
+                    quantidadeCachorro++;
+
+                }
+                else if (i.Tipo == "GATO")
+                {
+                    quantidadeGato++;
+                }
+                else
+                {
+                    quantidadePeixe++;
+                }
+            }
+            Console.WriteLine("A lista de animais de estimação possui as seguintes quantidades: ");
+            Console.WriteLine("Cachorro: " + quantidadeCachorro);
+            Console.WriteLine("Gato: " + quantidadeGato);
+            Console.WriteLine("Peixe: " + quantidadePeixe);
+        }
+
+        //com retorno return
+        public static string retornaQuantidade(List<Animal> lista)
+        {
+            int quantidadeCachorro = 0;
+            int quantidadeGato = 0;
+            int quantidadePeixe = 0;
+
+            foreach (Animal i in lista)
+            {
+                if (i.Tipo == "CACHORRO")
+                {
+                    quantidadeCachorro++;
+
+                }
+                else if (i.Tipo == "GATO")
+                {
+                    quantidadeGato++;
+                }
+                else
+                {
+                    quantidadePeixe++;
+                }
+            }
+            return " Cachorro: " + quantidadeCachorro +
+                "\n Gato: " + quantidadeGato +
+                "\n Peixe: " + quantidadePeixe;
+
+        }
+    }
+}
---
+++ POO/Exercicio02/Program.cs
@@ -0,0 +1,47 @@
+﻿//#2
+//Crie um programa em C# no qual o usuário deverá informar o nome e o tipo de cinco animais de estimação. 
+//O programa deverá exibir na tela para o usuário quantos Cachorros, Gatos e Peixes foram informados.
+
+//Regras que deverão ser seguidas para a implementação do programa:
+
+//Os únicos tipos de animais válidos são Cachorro, Gato, Peixe.
+//Caso seja informado um tipo diferente o programa repetir até o usuário digitar um tipo válido.
+//É obrigatório criar uma classe para representar o Animal.
+//A classe deverá possuir dois dados privados (propriedades) para representar as características do animal.
+//A classe deverá possuir métodos de acesso (propriedade getter e setter) para permitir que o
+//usuário armazene/leia os dados dos dois dados privados (propriedades).
+
+using Exercicio02;
+List<Animal> listaAnimais= new List<Animal>();
+string nome;
+string tipo;
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Clear();
+    Console.WriteLine("Vamos analisar os animais de estimação?" + i);
+    Console.Write("Digite o nome do animal: ");
+    nome = Console.ReadLine().ToUpper();
+    //Console.Write("Digite o tipo de animal: [Cachorro, Gato ou Peixe]: ");
+    //tipo = Console.ReadLine().ToUpper();
+    do
+    {
+        Console.Clear();
+        Console.Write("Digite o tipo de animal: [Cachorro, Gato ou Peixe]: ");
+        tipo = Console.ReadLine().ToUpper();
+        if (!Animal.verificaTipo(tipo))
+        {
+            Console.WriteLine("Tipo de animal inválido --> Serão aceitos somente Cachorros, Gatos e Peixes.");
+            Console.ReadKey();
+        }
+       
+    } while (!Animal.verificaTipo(tipo));
+    
+    listaAnimais.Add(new Animal(nome, tipo));
+    //Console.ReadKey();
+}
+
+Animal.Quantidade(listaAnimais);
+
+Console.WriteLine("As quantidades são: ");
+Console.WriteLine(Animal.retornaQuantidade(listaAnimais));
