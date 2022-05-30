e2d2a7ee27ca890facab117a3457ccd26e2102e9
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-05-30 12:01:15 -0300

    Reorganizando pastas dos exercícios de POO

---
+++ POO/Classes_exemplos/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("A ideia é trabalhar com métodos/funções de algumas classes importantes: string, Random, DateTime, List");
+
+
+//Classe string e seus principais métodos/atributos: Length, ToUpper(), ToLower(), Split(), Contains(), IndexOf()
+string frase = "Excelente turma de dotNet da Academia Atos";
+
+int tamanhoFrase = frase.Length;
+string fraseEmMaiusuculo = frase.ToUpper();
+string fraseEmMinusculoa = frase.ToLower();
+
+string[] palavrasNaFrase = frase.Split(" "); //separa cada palavra da frase em uma posicao do vetor palavrasNaFrase
+
+
+bool estaContido = frase.Contains("Atos"); //retorna se uma substring está ou não contida numa outra string
+int posicao = frase.IndexOf("Atos"); //retorna a posição em índice da primeira ocorrênciade uma substring na frase, 
+
+
+//Classe Random
+Random rnd = new Random(); //declarando uma 'variável' chamada rnd e instanciando em memória - new()
+
+int sorteio = rnd.Next(1, 11);
+Console.WriteLine(sorteio);
+
+
+char letra = (char)rnd.Next(65, 91); //A..Z 
+Console.WriteLine(letra);
+
+//Classe DateTime
+DateTime dataLocal = DateTime.Now;
+Console.WriteLine(dataLocal.ToString());
+string[] dataHora = dataLocal.ToString().Split(" ");
+
+Console.WriteLine("So a data: " + dataHora[0]);
+Console.WriteLine("So a hora: " + dataHora[1]);
+
+//Classe List - estrutura de dados que armazena outros objetos de maneira dinâmica
+
+List<string> listaAlunos = new List<string>(); //declarando uma 'variável' chamada listaAluno e instanciando em memória - new()
+
+listaAlunos.Add("Rodrigo");
+listaAlunos.Add("Andréa");
+listaAlunos.Add("Paulino");
+listaAlunos.Add("Leonardo");
+listaAlunos.Add("Flávia");
+listaAlunos.Add("Leandro");
+
+
+Console.WriteLine("Tamanho da lista: " + listaAlunos.Count);
+Console.WriteLine("Removendo o Leonardo");
+
+Console.WriteLine("Posição do Leonardo na lista: " + listaAlunos.IndexOf("Leonardo"));
+
+listaAlunos.Remove("Leonardo");
+Console.WriteLine("Tamanho da lista: " + listaAlunos.Count);
+
+//Console.WriteLine("Removendo todos os elementos da lista");
+//listaAlunos.Clear();
+//Console.WriteLine("Tamanho da lista: " + listaAlunos.Count);
+
+
+List<int> listaGlicemia = new List<int>();
+listaGlicemia.Add(136);
+listaGlicemia.Add(66);
+listaGlicemia.Add(78);
+listaGlicemia.Add(236);
+listaGlicemia.Add(90);
+listaGlicemia.Add(100);
+listaGlicemia.Add(111);
+
+listaGlicemia.Sort();
+
+Console.WriteLine("Menor medição de glicemia do período: " + listaGlicemia[0]);
+Console.WriteLine("Maior medição de glicemia do período: " + listaGlicemia[listaGlicemia.Count - 1]);
+
---
+++ POO/Pessoa/Program.cs
@@ -0,0 +1,195 @@
+﻿/*
+ * •Escreva uma classe Pessoa contendo todos os 
+ * atributos de uma pessoa. Faça métodos para apresentar os dados.
+ */
+
+string nome_digitado;
+string cpf_digitado;
+string telefone_digitado;
+string email_digitado;
+string endereco_digitado;
+
+
+Console.WriteLine("Digite um nome: ");
+nome_digitado = Console.ReadLine();
+
+Console.WriteLine("Digite o cpf: ");
+cpf_digitado = Console.ReadLine();
+
+Console.WriteLine("Digite o email: ");
+email_digitado = Console.ReadLine();
+
+Console.WriteLine("Digite o telefone: ");
+telefone_digitado = Console.ReadLine();
+
+Console.WriteLine("Digite o endereço: ");
+endereco_digitado = Console.ReadLine();
+
+
+
+Pessoa pessoa = new Pessoa();
+//pessoa.nome = nome_digitado; ==>  passa direto valor para o atributo
+//F12 vai para o metodo
+
+pessoa.Cadastrar(nome_digitado, cpf_digitado, telefone_digitado, email_digitado, endereco_digitado);
+
+Console.WriteLine("Pessoa = " + pessoa.nome + " | " + pessoa.cpf + " | " + pessoa.telefone + " | \n" +
+    pessoa.email + " | " + pessoa.endereco);
+
+pessoa.Dormir();
+Console.WriteLine("Está dormindo? " + pessoa.estaDormindo);
+
+//pessoa.Comer();
+//Console.WriteLine("Está comendo? " + pessoa.estaComendo);
+
+pessoa.Estudar();
+Console.WriteLine("O que a pessoa está fazendo???  Estudando??? " + pessoa.estaEstudando);
+
+int opcao;
+Console.WriteLine("O que a pessoa está fazendo agora??????");
+Console.WriteLine("1 - Dormindo?");
+Console.WriteLine("2 - Comendo?");
+Console.WriteLine("3 - Trabalhando?");
+Console.WriteLine("4 - Estudando?");
+Console.WriteLine("5 - Exercitando?");
+Console.WriteLine("Opção: ");
+opcao = int.Parse(Console.ReadLine());
+
+pessoa.Fazer(opcao);
+Console.WriteLine("A pessoa está....... " + pessoa.estaFazendo);
+
+Console.WriteLine("FIM PRIMEIRO TESTE \n");
+
+pessoa.Nome(nome_digitado);
+pessoa.CPF(cpf_digitado);   
+//pessoa.Telefone(telefone_digitado);
+//pessoa.Email(email_digitado);
+//pessoa.Endereco(endereco_digitado);
+
+Console.WriteLine("\n Os métodos estão apresentando os seguintes dados: ");
+Console.WriteLine("Pessoa: " + pessoa.nome);
+Console.WriteLine("CPF: " + pessoa.cpf);
+Console.WriteLine("Telefone: " + pessoa.Telefone(telefone_digitado));
+Console.WriteLine("Email: " + pessoa.Email(email_digitado));
+Console.WriteLine("Endereco:" + pessoa.Endereco(endereco_digitado));
+
+
+
+class Pessoa
+{
+    public string nome;
+    public string cpf;
+    public string telefone;
+    public string email;
+    public string endereco;
+    public bool estaDormindo;
+    public bool estaComendo;
+    public bool estaTrabalhando;
+    public bool estaEstudando;
+
+    public string estaFazendo;
+    //prop tab tab - atalho atributo
+    //public int MyProperty { get; set; }
+
+    //ctor - atalho construtor
+    //public Pessoa()
+    //{
+
+    //}
+
+    public void Cadastrar(string nome, string cpf, string telefone, string email, string encdereco)
+    {
+        this.nome = nome.ToUpper();
+        this.cpf = cpf.ToUpper();
+        this.telefone = telefone;
+        this.email = email.ToUpper();
+        this.endereco = encdereco.ToUpper();
+
+    }
+
+    public string Fazer(int opcao)
+    {
+        switch (opcao)
+        {
+            case 1:
+                estaFazendo = "Dormindo";
+                break;
+            case 2:
+                estaFazendo = "Comendo";
+                break;
+            case 3:
+                estaFazendo = "Trabalhando";
+                break;
+            case 4:
+                estaFazendo = "Estudando";
+                break;
+            case 5:
+                estaFazendo = "Exercitando";
+                break;
+            default:
+                break;
+        }
+        return estaFazendo;
+    }
+    public void Dormir()
+    {
+        estaDormindo = true;
+    }
+
+    public bool Comer()
+    {
+        estaComendo = true;
+        return estaComendo;
+    }
+
+    public void Acordar()
+    {
+        estaDormindo = false;
+    }
+
+    public void Trabalhar()
+    {
+        estaDormindo = false;
+        estaComendo = false;
+        estaEstudando = false;
+
+        estaTrabalhando = true;
+    }
+
+    public void Estudar()
+    { 
+        estaComendo = false;
+        estaDormindo = false;
+        estaTrabalhando = false;
+
+        estaEstudando=true;
+    }
+
+    //método para apresentar dados
+    public string Nome(string nm)
+    {
+     //nome = nm.ToUpper(); or
+     this.nome = nm.ToUpper();
+     return nome;
+    }
+    public string CPF(string cpf)
+    {
+        this.cpf = cpf.ToUpper();
+        return cpf;
+    }
+    public string Telefone(string tel)
+    {
+        telefone = tel.ToUpper();   
+        return telefone;
+    }
+    public string Email(string email)
+    {
+        email = email.ToLower();  
+        return email;
+    }
+    public string Endereco(string end)
+    {
+        endereco = end.ToUpper();
+        return endereco;
+    }
+}
