7458f8d9b59f2492de818e3ddb4e68f8e06be351
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-06-03 09:03:46 -0300

    vendas e netEmpresta

---
+++ 09_BancoDados/NetEmpresta/Program.cs
@@ -0,0 +1,69 @@
+﻿NetEmpresta possui ao redor de 200.000 filmes (filmes, documentários, animes, ..), cujo empréstimo deve ser controlado.
+
+Cada filme possui um número. 
+Para cada filme, é necessário saber seu título e sua categoria (comédia, drama, aventura, ...).
+Cada filme recebe um identificador próprio. 
+
+Os clientes podem desejar encontrar os filmes estrelados pelo seu ator predileto. 
+
+Por isso, é necessário manter a informação dos atores que estrelam em cada filme. 
+Nem todo filme possui estrelas. Para cada ator os clientes às vezes desejam saber o nome real,
+bem como a data de nascimento.
+
+A NetEmpresa possui muitos clientes cadastrados. 
+Somente clientes cadastrados podem alugar filmes. 
+Para cada cliente é necessário saber seu pré-nome e seu sobrenome, seu telefone e seu endereço. 
+Além disso, cada cliente recebe um número de associado.
+Finalmente, desejamos saber que filmes cada cliente tem alugados. 
+Um cliente pode ter vários empréstimos em um instante no tempo. 
+
+Nota: são mantidos registros históricos de aluguéis.
+
+A partir desse relato construa:
+- modelo lógico
+
+Indicando entidades, atributos, relacionamentos, cardinalidades
+
+
+public class Categoria
+{
+    int codCategoria;
+    string nome;
+}
+public class Filme {
+    int codFilme;
+    string titulo;
+    Categoria categoria;
+}
+
+public class Artista {  // ou estrelas???
+    int codArtista;
+    string nome;
+    string nomeReal;
+    DateTime dataNascimento;
+
+}
+
+public class FilmeArtista { // nxn
+    Filme filme;
+    Artista artista;    
+}
+
+public class Cliente {
+    int codAssociado;
+    string preNome;
+    string sobrenome;
+    string rua;
+    int numero;
+    string complemento;
+    string bairro;
+    int cep;
+    string cidade;
+    int telefone;
+}
+
+public class Aluguel { //n filmes x n clientes
+    Cliente cliente;
+    Filme filme;
+    DateTime dataHoraAluguel;
+}
---
+++ 09_BancoDados/modelagemVendas/Program.cs
@@ -0,0 +1,124 @@
+﻿//Modelagem Lógica do Sistema de Vendas
+
+//Cliente(codCliente, nome, rua, numero, complemento, cep, cidade, estado, fone)
+
+//Vendedor(codVendedor, nome, fone, senha)
+
+//Prateleira(codPrateleira, localizacao)
+
+//Produto(codProduto, codigoBarras, descricao, codPrateleira)
+//    codPrateleira referencia Prateleira
+
+//Venda(dataNotafiscal, numeroNotaFiscal, codProduto, codVendedor, codCliente)
+//    codProduto referencia Produto
+//    codVendedor referencia Vendedor
+//    codCliente referencia Cliente
+
+
+//Entidade = classe
+//Atributo/Coluna se for uma chave estrangeira, no diagrama fica subentendido
+//Relacionamento indica quem visita quem (chave primária visita outra entidade  
+//                                        como chave estrangeira)
+//    -Uma chave estrangeira deve ser uma chave primária na sua entidade/classe
+//    de origem
+//Cardinalidade: mostra como se dará a visita (quem terá a chave estrangeira)
+
+
+public class Cliente 
+{
+    int codCliente;
+    string nome;
+    string rua;
+    int numero;
+    string complemento;
+    int cep;
+    string cidade;
+    string estado;
+    int fone;
+
+    public Cliente()
+    {
+
+    }
+
+    public int CodCliente { get => codCliente; set => codCliente = value; }
+    public string Nome { get => nome; set => nome = value; }
+    public string Rua { get => rua; set => rua = value; }
+    public int Numero { get => numero; set => numero = value; }
+    public string Complemento { get => complemento; set => complemento = value; }
+    public int Cep { get => cep; set => cep = value; }
+    public string Cidade { get => cidade; set => cidade = value; }
+    public string Estado { get => estado; set => estado = value; }
+    public int Fone { get => fone; set => fone = value; }
+}
+
+public class Vendedor
+{
+    int codVendedor;
+    string nome;
+    int fone;
+    string senha;
+
+    public Vendedor()
+    {
+
+    }
+
+    public int CodVendedor { get => codVendedor; set => codVendedor = value; }
+    public string Nome { get => nome; set => nome = value; }
+    public int Fone { get => fone; set => fone = value; }
+    public string Senha { get => senha; set => senha = value; }
+}
+
+public class Prateleira  //n produtos
+{
+    int codPrateleira;
+    string localizacao;
+
+    public Prateleira()
+    {
+
+    }
+
+    public int CodPrateleira { get => codPrateleira; set => codPrateleira = value; }
+    public string Localizacao { get => localizacao; set => localizacao = value; }
+}
+
+public class Produto
+{
+    int codProduto;
+    int CodigoBarras;
+    string descricao;
+    Prateleira prateleira;
+
+    public Produto()
+    {
+
+    }
+
+    public int CodProduto { get => codProduto; set => codProduto = value; }
+    public int CodigoBarras1 { get => CodigoBarras; set => CodigoBarras = value; }
+    public string Descricao { get => descricao; set => descricao = value; }
+    public Prateleira Prateleira { get => prateleira; set => prateleira = value; }
+}
+
+public class Venda 
+{
+    int numeroNotaFiscal; //chave primária
+    DateTime dataNotaFiscal;
+    Produto produto;
+    Vendedor vendedor;
+    Cliente cliente;
+
+    public Venda()
+    {
+
+    }
+
+    public int NumeroNotaFiscal { get => numeroNotaFiscal; set => numeroNotaFiscal = value; }
+    public DateTime DataNotaFiscal { get => dataNotaFiscal; set => dataNotaFiscal = value; }
+    public Produto Produto { get => produto; set => produto = value; }
+    public Vendedor Vendedor { get => vendedor; set => vendedor = value; }
+    public Cliente Cliente { get => cliente; set => cliente = value; }
+}
+
