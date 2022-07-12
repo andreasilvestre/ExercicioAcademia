ab393514932e85c6a15bbc9aa705e93dd759eb0b
andreasilvestre <andrea.silvestre@yahoo.com.br>
2022-07-12 12:13:54 -0300

    primeiro MVC

---
+++ MVCMovie/Controllers/OlaMundo_Controller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCMovie.Controllers
+{
+    public class OlaMundo_Controller : Controller
+    {
+        //public string Index()
+        //{
+        //    return "Minha ação padrão";
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string BemVindo(string nome, int vezes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"oi{ nome}, vezes: { vezes}");
+            //return "Bem vindo ao meu método de boas vindas do meu controller";
+        }
+        public string Teste()
+        {
+            return "Aqui mostra a relação rota x método";
+        }
+    }
+
+}
